776babea8940b63d058e6f19c00cae8655f8efeb
EgleJur <115785833+EgleJur@users.noreply.github.com>
2023-01-17 23:13:07 +0200

    Add files via upload

--- src/pages/AnimalList.test.js
+++ src/pages/AnimalList.test.js
@@ -1,1 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react';
+import { AnimalListPage } from './AnimalList';
+
+test('Animal List initial render', () => {
+    render(<AnimalListPage />);
+    const title = screen.getByText('Animals');
+    expect(title).toBeDefined();
+});
